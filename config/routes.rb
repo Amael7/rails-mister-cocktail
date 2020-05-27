66b9dc3fe4a3821472a63d0067ca6fcd4580fd5b
stephanemontoro <stephane.montoro@hotmail.com>
2020-05-27 21:15:04 +0200

    everything is finished except reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails, only: %i[index show new create]
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: [:create]
+  end
+
+  resources :doses, only: [:destroy]
 end
